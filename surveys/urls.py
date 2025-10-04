43cb0fedd164161d9fcf8d67bc651ab447df8d13
Jean Carlos <jeancatovio23@gmail.com>
2025-10-03 23:26:31 -0500

    feat: Mejoras en la interfaz y encuestas públicas
      Este commit introduce varias mejoras en la interfaz de usuario y añade la funcionalidad de encuestas
      públicas.
    
      Los cambios incluyen:
    
       - Mejora visual del login: Se ha rediseñado la página de inicio de sesión para que sea más atractiva
         visualmente. El formulario ahora está centrado, tiene un estilo más moderno y cuenta con una animación
         sutil para una mejor experiencia de usuario.
       - Encuestas públicas: Se ha añadido una nueva sección de encuestas públicas que no requiere autenticación.
         Los usuarios anónimos ahora pueden ver y responder encuestas. La ruta para esta nueva sección es
         /surveys/public/.
       - Ocultar botones de autenticación: En las páginas de encuestas públicas, los botones de "Ingresar" y
         "Crear cuenta" ahora se ocultan para evitar confusiones.
       - Estilo de los radio buttons: Se ha mejorado el estilo de las preguntas de tipo "radio button" para que
         las opciones se muestren en una sola fila, optimizando el espacio y la legibilidad.

--- surveys/urls.py
+++ surveys/urls.py
@@ -1,4 +1,5 @@
 ﻿
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
@@ -6,6 +7,10 @@ app_name = "surveys"
 
 urlpatterns = [
     path("", views.survey_list, name="list"),
+    path("public/", views.survey_list_public, name="public_list"),
     path("s/<slug:code>/", views.survey_fill, name="fill"),
     path("s/<slug:code>/check-respondent/", views.check_duplicate_respondent, name="check_respondent"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", auth_views.LoginView.as_view(template_name="surveys/auth_login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
