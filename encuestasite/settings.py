1c73003b0e066f114d8a866c98d87c85d11eb190
Jean Carlos <jeancatovio23@gmail.com>
2025-10-06 22:29:37 -0500

    fix logo and style quetions

--- encuestasite/settings.py
+++ encuestasite/settings.py
@@ -61,6 +61,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'surveys:list'
